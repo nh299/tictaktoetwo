9da34a1aceb88d12d773f1b3152bc62aec4621a0
Nick Harris <94771240+nh299@users.noreply.github.com>
2023-01-23 16:13:36 -0700

    Add project files.

---
+++ tictaktoe2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace tictaktoe2
+{
+    class Program
+    {
+        public static class Game
+        {
+            public static void WinGame(string[] board) {
+                bool win = false;
+                string winnerChar = "";
+
+                if ((board[0] == board[1] && board[1] == board[2]) && (board[0] != " "))
+                {
+                    win = true;
+                    winnerChar = board[0];
+                }
+                else if ((board[3] == board[4] && board[4] == board[5]) && (board[3] != " "))
+                {
+                    win = true;
+                    winnerChar = board[3];
+                }
+                else if ((board[0] == board[3] && board[3] == board[6]) && (board[0] != " "))
+                {
+                    win = true;
+                    winnerChar = board[0];
+                }
+                else if ((board[6] == board[7] && board[7] == board[8]) && (board[6] != " "))
+                {
+                    win = true;
+                    winnerChar = board[6];
+                }
+                else if ((board[1] == board[4] && board[4] == board[7]) && (board[1] != " "))
+                {
+                    win = true;
+                    winnerChar = board[1];
+                }
+                else if ((board[2] == board[5] && board[5] == board[8]) && (board[2] != " "))
+                {
+                    win = true;
+                    winnerChar = board[2];
+                }
+                else if ((board[0] == board[4] && board[4] == board[8]) && (board[0] != " "))
+                {
+                    win = true;
+                    winnerChar = board[0];
+                }
+                else if ((board[2] == board[4] && board[4] == board[8]) && (board[2] != " "))
+                {
+                    win = true;
+                    winnerChar = board[2];
+                }
+                if (win == true)
+                {
+                    Console.Write(winnerChar + " is the winner!\n");
+                }
+            }
+            public static void PrintBoard(string[] board) {
+                for (int i = 0;i < board.Length;i++)
+                    {
+                    if (i == 2 || i == 5)
+                    {
+                        Console.Write(board[i] + "\n");
+                    }
+                    else
+                    {
+                        Console.Write(board[i]);
+                    }
+                    }
+            }
+        }
+        public static void Main(string[] args)
+        {
+            string[] myArray = { "o", "p", "o", "q", "o", "e" , "r", "t", "o"};
+            //Game newGame = new Game();
+            //newGame.WinGame(myArray);
+            Game.WinGame(myArray);
+            Game.PrintBoard(myArray);
+        }
+    }
+}
