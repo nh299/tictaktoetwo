6d84df7fc737a0f2ee107a049082a7603ccdfbf7
Benjamin Hirschi <hirschiben@gmail.com>
2023-01-25 14:42:21 -0700

    Added comments and also fixed a bug that declared a winner at positions 2, 4, and 8 but now it is changed to show a winner if 2, 4, and 6 are the same character

--- tictaktoe2/Game.cs
+++ tictaktoe2/Game.cs
@@ -5,8 +5,12 @@ using System.Text;
 namespace tictaktoe2
 {
 
+    // game class
         public static class Game
         {
+            // this method WinGame takes a string array to check to see the possible options of 3 in
+            // a row, to see if a player has won the game. It keeps track of the winning character (x or o)
+            // to be displayed at the end so the winner is identified
             public static string WinGame(string[] board)
             {
                 bool win = false;
@@ -47,13 +51,14 @@ namespace tictaktoe2
                     win = true;
                     winnerChar = board[0];
                 }
-                else if ((board[2] == board[4] && board[4] == board[8]))
+                else if ((board[2] == board[4] && board[4] == board[6]))
                 {
                     win = true;
                     winnerChar = board[2];
                 }
                 if (win == true)
                 {
+                // this is the output if there is a winner
                     return winnerChar + " is the winner!\n";
                 }
                 else
@@ -61,16 +66,20 @@ namespace tictaktoe2
                     return "";
                 }
             }
+               // This method inherits a string array and prints the tic tac toe board according to the values in that array
+               // it doesn't return a value, just prints the board to the screen
             public static void PrintBoard(string[] board)
             {
                 for (int i = 0; i < board.Length; i++)
                 {
+                    // if the array is at position 2, 5, or 8 a newline is needed to begin the next row of the board
                     if (i == 2 || i == 5 || i == 8)
                     {
                         Console.Write(board[i] + "\n");
                     }
                     else
                     {
+                    // This '|' creates the outline of the tic tac toe board
                         Console.Write(board[i] + "|");
                     }
                 }
--- tictaktoe2/Program.cs
+++ tictaktoe2/Program.cs
@@ -7,6 +7,9 @@ namespace tictaktoe2
         static void Main(string[] args)
         {
 
+            // initialize vars, turn indicates if we display the welcome message, 
+            // win of false means continue with the game, position is the player's choice of where to put their X or O
+            // initialize the board array so it displays simply the options for players to choose for their turn
             int turn = 0;
             bool win = false;
             int position;
@@ -14,15 +17,19 @@ namespace tictaktoe2
 
             Console.WriteLine("Welcome to Tic-Tac-Toe!");
 
+            // Loop through this until someone has won the game
             while (win == false) 
             {
                 if (turn == 0)
                 {
-                    Console.WriteLine("Player one choose a position");
+                    // Receive as input player 1's choice position
+                    Console.WriteLine("Player one (X) choose a position");
+                    //call the printboard method from the game class to display the board of options for moving
                     Game.PrintBoard(board);
 
                     position = int.Parse(Console.ReadLine());
 
+                    // Make sure a player has not already chosen the desired square in a previous turn
                     if (board[position - 1] != "X" && board[position - 1] != "O")
                     {
                         board[position - 1] = "X";
@@ -32,12 +39,14 @@ namespace tictaktoe2
                         Console.WriteLine("That spot is already taken. Please choose an open spot.");
                     }
 
+                    // incrememnt the number of turns 
                     turn = (turn + 1) % 2;
 
                 }
                 else
                 {
-                    Console.WriteLine("Player two choose a position");
+                    // same process for player two as above for player 1
+                    Console.WriteLine("Player two (O) choose a position");
                     Game.PrintBoard(board);
 
                     position = int.Parse(Console.ReadLine());
@@ -53,13 +62,20 @@ namespace tictaktoe2
 
                     turn = (turn + 1) % 2;
                 }
+
+                // check to see if either player has won the game with the last move with the method in the game class
                 string output = Game.WinGame(board);
+
+                // output is initialized as "" and only changes when someone has won
                 if (output == "")
                 {
                     win = false;
                 }
                 else
                 {
+
+                    // if the method returns an output, someone has won, so we change win to true to get out
+                    // of the while loop and print the final board along with the output of who won!
                     win = true;
                     Game.PrintBoard(board);
                     Console.WriteLine(output);
